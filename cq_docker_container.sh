12b5be74f12492ab408c0048d46e6c7422f6aa6a
RyuBAI <me@ryubai.com>
2019-08-28 13:48:10 +0800

    写一行太丑了

--- cq_docker_container.sh
+++ cq_docker_container.sh
@@ -2,4 +2,14 @@
 docker pull richardchien/cqhttp:latest
 mkdir /root/coolq-data
 
-docker run -d -ti --name coolq -v /root/coolq-data:/home/user/coolq -p 9000:9000 -p 5700:5700 -p 5800:5800 -e VNC_PASSWD=password -e COOLQ_URL=http://dlsec.cqp.me/cqp-tuling -e CQHTTP_POST_URL=http://127.0.0.1:5800 -e CQHTTP_SERVE_DATA_FILES=yes richardchien/cqhttp:latest
+docker run -tid \
+ --name coolq \
+ -v /root/coolq-data:/home/user/coolq \
+ -p 9000:9000 \
+ -p 5700:5700 \
+ -p 5800:5800 \
+ -e VNC_PASSWD=password \
+ -e COOLQ_URL=http://dlsec.cqp.me/cqp-tuling \
+ -e CQHTTP_POST_URL=http://127.0.0.1:5800 \
+ -e CQHTTP_SERVE_DATA_FILES=yes \
+ richardchien/cqhttp:latest
